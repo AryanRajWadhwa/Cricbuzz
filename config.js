620394af88c369e5b185330353a88168c904c370
AryanRajWadhwa <aryanrajwadhwa@gmail.com>
2023-09-14 12:30:53 +0530

    Initial Commit

--- config.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a random 64-character secret key
-const secretKey = crypto.randomBytes(32).toString('hex');
-
-const jwtSecretKey = secretKey
-module.exports = { jwtSecretKey }
